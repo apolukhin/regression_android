8c7420f137301a10c0bb535ef68ca9f201e15db7
Antony Polukhin <antoshkka@gmail.com>
2014-01-22 15:01:30 +0400

    Use 'android' for 'target-os'. Simplify configuration. Added license notices and license.

--- linux/run.sh
+++ linux/run.sh
@@ -1,3 +1,9 @@
+# Copyright (c) 2013 Antony Polukhin
+#
+# Distributed under the Boost Software License, Version 1.0. (See
+# accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
 LAUNCHER=`pwd`/run_on_android.sh
 
 mkdir build
@@ -10,4 +16,4 @@ rm run.py
 wget https://raw.github.com/boostorg/boost/develop/tools/regression/src/run.py
 
 chmod +x run.py
-./run.py "--bjam-options=testing.launcher=${LAUNCHER} -j4 -d+4 threadapi=pthread target-os=linux toolset=${TOOLSETNAME}" --tag=${TAGNAME} --bjam-toolset=gcc-toolsets --pjl-toolset=gcc-toolsets --runner=${RUNNERNAME} --incremental
+./run.py "--bjam-options=testing.launcher=${LAUNCHER} -j4 -d+4 threadapi=pthread target-os=android toolset=${TOOLSETNAME}" --tag=${TAGNAME} --bjam-toolset=gcc-toolsets --pjl-toolset=gcc-toolsets --runner=${RUNNERNAME} --incremental
--- linux/run_on_android.sh
+++ linux/run_on_android.sh
@@ -1,3 +1,9 @@
+# Copyright (c) 2013 Antony Polukhin
+#
+# Distributed under the Boost Software License, Version 1.0. (See
+# accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
 BINARY_NAME=${1##*/}
 ANDROID_EXEC_DIR=/data/local/tmp/
 
